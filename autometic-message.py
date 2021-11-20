04f3adeee37144bf977a71a0195358a930207ad2
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-20 16:54:30 +0600

    Add files via upload

---
+++ autometic-message.py
@@ -0,0 +1,3 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+8801784089538','Hello check message',11,40) #('number','what message','time')
+pywhatkit.sendwhats_image('+8801705934642','Love-From-Kaochar.jpg','Happy Birthday To U Dada')
